1e0013e2f24c3d69cc7e1726c1d6c333b5ac468c
namxle <namle@genetics.vn>
2022-06-07 10:07:22 +0700

    adding routes

---
+++ chat-service/config/routes.js
@@ -0,0 +1,22 @@
+/**
+ * Route Mappings
+ * (sails.config.routes)
+ *
+ * Your routes tell Sails what to do each time it receives a request.
+ *
+ * For more information on configuring custom routes, check out:
+ * https://sailsjs.com/anatomy/config/routes-js
+ */
+
+module.exports.routes = {
+
+    // '/': 'TestController.test',
+
+    '/test-rabbitmq': 'TestController.testRabbitMQ',
+
+    '/subscribe': 'ChatController.subscribe',
+
+    '/generate-iwt': 'TestController.getJWT',
+
+    '/send': 'ChatController.send'
+};

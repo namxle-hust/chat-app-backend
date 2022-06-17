67a7d32c522a7425f062db809f55694af34b829d
thuhienbk0301 <thuhien.bk0301@gmail.com>
2022-06-18 00:04:02 +0700

    response service in web service

---
+++ web-service/api/services/ResponseService.js
@@ -0,0 +1,74 @@
+/**
+**/
+
+'use strict'
+
+module.exports = {
+	/**
+	 * Generate custom error
+	 * @param  {string} msg
+	 * @param  {object} data
+	 * @return {Error}
+	 */
+
+	customError: function (msg) {
+		let error = new Error(msg)
+
+		error.isCustomError = 1
+
+		return error;
+	},
+
+	isCustomError: function (error) {
+		error = error ? error : {}
+
+		return error.isCustomError == 1
+	},
+
+	/**
+	 * Error Response
+	 * @param  {Object} res  Sails Request Object
+	 * @param  {Mix} data
+	 * @param  {Mix} code
+	 */
+	error: function (res, message, code) {
+		let content = {
+			status: 'error'
+		}
+
+		if (message) {
+			content.message = message;
+		} else {
+			content.message = "Uknown Error";
+		}
+
+		code = code || 200
+
+		return res.status(code).json(content)
+	},
+
+	/**
+	 * Success Response
+	 * @param  {Object} res  Sails Request Object
+	 * @param  {Mix} data
+	 * @param  {Mix} code
+	 */
+	success: function (res, data, message, code) {
+		let content = {
+			status: 'success'
+		}
+
+        if (data) {
+            content.data = data;
+        }
+
+		if (message) {
+			content.message = message
+		}
+
+		code = code || 200
+
+		return res.status(code).json(content)
+	}
+
+}

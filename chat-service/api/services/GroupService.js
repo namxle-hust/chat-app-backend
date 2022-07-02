7ebe0264177f8c20f493c1e6a566f88cbdbaaece
namxle <namle@genetics.vn>
2022-07-02 16:22:25 +0700

    send group message

--- chat-service/api/services/GroupService.js
+++ chat-service/api/services/GroupService.js
@@ -3,14 +3,17 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = {
-	send: async (qmsg, group_id) => {
+	send: async (qmsg, group_id, user_sent_id) => {
 		try {
-			let users = Groups.find({ id: group_id });
+			let groupMembers = GroupMemberships.find({ group_id: group_id });
 
 			await Promise.all(
-				users.map(async (user) => {
+				groupMembers.map(async (groupMember) => {
+                    if (groupMember.user_id == user_sent_id) {
+                        return;
+                    }
 					let queueMessage = JSON.parse(JSON.stringify(qmsg));
-					await QueueService.publish(user.id, queueMessage);
+					await QueueService.publish(groupMember.user_id, queueMessage);
 				})
 			);
 			return true;
@@ -18,4 +21,5 @@ module.exports = {
 			throw error;
 		}
 	},
+
 };
